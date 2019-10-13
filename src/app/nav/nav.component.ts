d05469df35111731db78f6104b2e898d9fe67d21
Mallerie Shirley <mallerieshirley@MacBook-Pro.attlocal.net>
2019-10-13 15:39:37 -0400

    add home contact and nav components to the app

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass']
+})
+export class NavComponent implements OnInit {
+  appTitle: string = 'myapp';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
